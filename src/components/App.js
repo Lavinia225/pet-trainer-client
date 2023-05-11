782d422cc6e5991b50bb9438bfdd281403628833
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-11 16:19:28 -0500

    Added pet and trainer states, as well as a useEffect hook to fetch both of them from the backend server

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,33 @@
-
+import {Route, Switch} from "react-router-dom"
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [trainers, setTrainers] = useState([])
+  const [pets, setPets] = useState([])
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/trainers")
+    .then(res => res.json())
+    .then(trainerArray => setTrainers(trainerArray))
+  }, [])
+
+  useEffect(()=>{
+    fetch("http://localhost:9292/pets")
+    .then(res => res.json())
+    .then(petArray => setPets(petArray))
+  }, [])
+
+console.log(trainers)
+console.log(pets)
+
   return (
     <div className="App">
-      
+      {/*NavBar*/}
+      <Switch>
+        <Route exact path="/">
+          <p>Placeholder text for nyaow.</p>
+        </Route>
+      </Switch>
     </div>
   );
 }

806afe0ae00722b452e38b8c1445e488296d60e2
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-15 14:21:24 -0500

    Added table headers

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,7 +1,19 @@
 function Pets(){
+    //name, trainer, species, breed, age, gender, personality, working on
     return(
         <table>
-
+            <tbody>
+                <tr>
+                    <th>Name: </th>
+                    <th>Species: </th>
+                    <th>Breed: </th>
+                    <th>Age: </th>
+                    <th>Gender: </th>
+                    <th>Personality: </th>
+                    <th>Trainer: </th>
+                    <th>Working On: </th>
+                </tr>
+            </tbody>
         </table>
     )
 }

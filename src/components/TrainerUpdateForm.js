15d029a720d73fa5302e4099041c1434280baaeb
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-15 17:27:10 -0500

    Added the first part of the controlled form

--- src/components/TrainerUpdateForm.js
+++ src/components/TrainerUpdateForm.js
@@ -1,7 +1,23 @@
-function  TrainerUpdateForm(){
+import {useState} from 'react'
+//Figure out how to have this assign and unassign pets | name,personality, payrate at the fields here
+function  TrainerUpdateForm({trainer}){
+    const [formData, setFormData] = useState({
+        name: trainer.name,
+        personality: trainer.personality,
+        payrate: trainer.payrate
+    })
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return(
         <form>
-
+            <label htmlFor="name">Name: </label>
+            <input type="text" value={formData.name} onChange={handleChange}></input>
         </form>
     )
 }

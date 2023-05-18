89e7cb013135811136dba1392eb1667f63122ad5
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-17 19:45:05 -0500

    Set up dynamic rendering for edit form

--- src/components/TrainerPage.js
+++ src/components/TrainerPage.js
@@ -1,9 +1,12 @@
 import {useEffect, useState} from 'react'
-import {useParams} from 'react-router-dom'
+import {useHistory, useParams} from 'react-router-dom'
+import TrainerUpdateForm from './TrainerUpdateForm'
 
 function TrainerPage(){
     const [trainer, setTrainer] = useState({})
+    const [editing, setEditing] = useState(false)
     const params = useParams()
+    const history = useHistory()
 
     useEffect(()=>{
         fetch(`http://localhost:9292/trainers/${params.id}`)
@@ -11,12 +14,28 @@ function TrainerPage(){
         .then(t => setTrainer(t))
     }, [])
 
+    function handleEditing(){
+        if(editing){
+            history.push(`/trainers/${trainer.id}`)
+        }
+        else{
+            history.push(`/trainers/${trainer.id}/edit`)
+        }
+        setEditing(!editing)
+    }
+
+    function handleSubmit(){
+
+    }
+
     return(
         <>
-            <h3><span>✎</span> {trainer.name}</h3>
+            {editing ? <TrainerUpdateForm cancelEdit={handleEditing} handleSubmit={handleSubmit} trainer={trainer}/> :<>
+            <h3><span onClick={handleEditing}>✎</span> {trainer.name}</h3>
             <p>{trainer.personality} personality・${trainer.payrate} per hour</p>
             <p>Is training the following pets: </p>
             {/*{trainer.map(pet => <Pet key={pet.id} pet={pet}/>)}*/}
+            </>}
         </>
     )
 }

3daef67e23565819e212e9308389f1726e3e5566
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-18 20:18:31 -0500

    Set up most of the form stuff

--- src/components/NewTrainerForm.js
+++ src/components/NewTrainerForm.js
@@ -1,6 +1,34 @@
+import {useState} from 'react'
+
 function NewTrainerForm(){
+    const [formData, setFormData] = useState({
+        name: trainer.name,
+        personality: trainer.personality,
+        payrate: trainer.payrate
+    })
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+    }
+    //Need a button to cancel trainer creation
     return(
         <>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name: </label>
+                <input type="text" name='name' value={formData.name} onChange={handleChange}></input>
+                <label htmlFor="personality">Personality: </label>
+                <input type="text" name='personality' value={formData.personality} onChange={handleChange}></input>
+                <label htmlFor="payrate">Payrate: </label>
+                <input type="number" name="payrate" value={formData.payrate} onChange={handleChange}></input>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }

740c4450fcab107be3047f23a9fae88bb4da897e
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-19 22:14:09 -0500

    Pets now render their names as links, similar to trainers

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,9 +1,9 @@
-
+import {Link} from 'react-router-dom'
 
 function Pet({pet}){
-     //name, trainer, species, breed, age, gender, personality, working on
+    
      function createTD(entry){
-        if(entry[0] === "id" || entry[0] === "trainer_id") return null
+        if(entry[0] === "id" || entry[0] === "trainer_id" || entry[0] === "name") return null
         else{
             return <td key={entry[1]}>{entry[1]}</td>
         }
@@ -11,6 +11,7 @@ function Pet({pet}){
 
     return(
         <tr>
+            <td><Link to={`/pets/${pet[0][1]}`}>{pet[1][1]}</Link></td>
             {pet.map(createTD)}
         </tr>
     )

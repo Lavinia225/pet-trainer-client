241d5fad113b794de9d565da771c94a97a303632
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-24 17:58:07 -0500

    Filled out default state in a way react likes better.

--- src/components/PetUpdateForm.js
+++ src/components/PetUpdateForm.js
@@ -2,16 +2,38 @@ import {useState, useEffect} from 'react'
 import { useParams } from "react-router-dom"
 
 function PetUpdateForm({handleUpdate, trainers}){
-    const [formdata, setFormdata] = useState({}) //name, trainer, species, breed, age, gender, personality, working on
+    const [formData, setFormData] = useState({
+        name: "",
+        trainer: "",
+        species: "",
+        breed: "",
+        age: 0,
+        gender: "",
+        personality: "",
+        workingOn: ""
+    })
     const params = useParams()
 
     useEffect(()=>{
         fetch(`http://localhost:9292/pets/${params.id}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(pet => setFormData(pet))
     },[])
-    return(<>
-    
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    return(
+    <>
+        <button>Cancel Editing</button>{/*Currently does nothing */}
+        <form>
+        <label htmlFor="name">Name: </label>
+        <input type="text" name='name' value={formData.name} onChange={handleChange}></input>
+        </form>
     </>)
 }
 

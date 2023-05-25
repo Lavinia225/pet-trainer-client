2cdf578d4b08d6c4b676fbdd3b083b42aceead4d
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-25 16:42:37 -0500

    There's a button that takes you to pet creation

--- src/components/Pets.js
+++ src/components/Pets.js
@@ -1,23 +1,27 @@
 import Pet from './Pet'
+import {Link} from 'react-router-dom'
 
 function Pets({pets, isTrainer = false}){
     //name, trainer, species, breed, age, gender, personality, working on
     return(
-        <table>
-            <tbody>
-                <tr>
-                    <th>Name: </th>
-                    <th>Species: </th>
-                    <th>Breed: </th>
-                    <th>Age: </th>
-                    <th>Gender: </th>
-                    <th>Personality: </th>
-                    <th>Working On: </th>
-                    {isTrainer ? null : <th>Trainer: </th>}
-                </tr>
-                {pets.map(pet => <Pet key={pet.id} pet={Object.entries(pet)}/>)}
-            </tbody>
-        </table>
+        <>
+            <Link to="/pets/create">Create New Pet</Link>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name: </th>
+                        <th>Species: </th>
+                        <th>Breed: </th>
+                        <th>Age: </th>
+                        <th>Gender: </th>
+                        <th>Personality: </th>
+                        <th>Working On: </th>
+                        {isTrainer ? null : <th>Trainer: </th>}
+                    </tr>
+                    {pets.map(pet => <Pet key={pet.id} pet={Object.entries(pet)}/>)}
+                </tbody>
+            </table>
+        </>
     )
 }
 

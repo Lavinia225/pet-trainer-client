b6a4e814744a097262b74a60cebddaeb9206f681
Luna Seifert <thepuddingdeity@gmail.com>
2023-05-25 16:58:54 -0500

    Created Delete request in trainers

--- src/components/TrainerPage.js
+++ src/components/TrainerPage.js
@@ -3,7 +3,7 @@ import {useHistory, useParams} from 'react-router-dom'
 import TrainerUpdateForm from './TrainerUpdateForm'
 import Pets from './Pets'
 
-function TrainerPage({handleUpdate}){
+function TrainerPage({handleUpdate, handleDelete}){
     const [trainer, setTrainer] = useState({})
     const [editing, setEditing] = useState(false)
     const params = useParams()
@@ -41,10 +41,26 @@ function TrainerPage({handleUpdate}){
         }
     }
 
+    function handleDelete(){
+        if(petChecker()){
+            window.alert("You must reassign all pets belonging to this trainer before firing them.")
+        }
+        else{
+            if(window.confirm("Are you sure you want to delete this entity?")){
+                fetch(`http://localhost:9292/trainers/${trainer.id}`, {
+                    method: "DELETE"
+                })
+                .then(history.push('/trainers'))
+            }
+        }
+    }
+
     return(
         <>
             {editing ? <TrainerUpdateForm cancelEdit={handleEditing} trainer={trainer} handleUpdate={handleUpdate} handlePageUpdate={handlePageUpdate}/> :<>
-            <h3><span onClick={handleEditing}>✎</span> {trainer.name}</h3>
+            <h3>
+                <span onClick={handleEditing}>✎</span> {trainer.name} <span onClick={handleDelete}>🗑</span>
+            </h3>
             <hr />
             <p>{trainer.personality} personality・${trainer.payrate} per hour</p>
             <p>Is training the following pets: </p>
